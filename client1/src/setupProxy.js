7e0a1dbc1b3d25c87c6c57afe52aefddf77dc369
Daylan <daylanberry@gmail.com>
2020-07-13 11:38:17 -0700

    config for deploy

--- client1/src/setupProxy.js
+++ client1/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api/**',
     createProxyMiddleware({
-      target: 'https://stock-simu.herokuapp.com/',
+      target: 'http://localhost:5010',
       changeOrigin: true
     })
   )
@@ -12,7 +12,7 @@ module.exports = function(app) {
   app.use(
     '/auth/google',
     createProxyMiddleware({
-      target: 'https://stock-simu.herokuapp.com/'
+      target: 'http://localhost:5010'
     })
   )
 };
