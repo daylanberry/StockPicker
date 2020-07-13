3adf2e8450d75abc213627b680ad4d5cba19e0da
Daylan <daylanberry@gmail.com>
2020-07-13 13:26:49 -0700

    config for deploy

--- keys/prod.js
+++ keys/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GOOGLE_SECRET_KEY: "JdtbBj90VrNAlQDdLZnPJRNv",
-  GOOGLE_CLIENT_ID: "507256132337-1b55e05gb7902amr37rioheh6paqc2nr.apps.googleusercontent.com",
-  MONGO_URI: "u0b2XKHcdnVeelkt",
-  COOKIE_KEY: 'secret'
+  googleSecretKey: process.env.GOOGLE_SECRET_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 }
