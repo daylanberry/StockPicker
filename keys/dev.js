e72790bb785c66322adf0a7735d55403b76dbe7b
Daylan <daylanberry@gmail.com>
2020-07-13 13:16:59 -0700

    config for deploy

---
+++ keys/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleSecretKey: "JdtbBj90VrNAlQDdLZnPJRNv",
+  googleClientID: "507256132337-1b55e05gb7902amr37rioheh6paqc2nr.apps.googleusercontent.com",
+  mongoURI: "u0b2XKHcdnVeelkt",
+  cookieKey: 'secret'
+}
---
+++ keys/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_SECRET_KEY: "JdtbBj90VrNAlQDdLZnPJRNv",
+  GOOGLE_CLIENT_ID: "507256132337-1b55e05gb7902amr37rioheh6paqc2nr.apps.googleusercontent.com",
+  MONGO_URI: "u0b2XKHcdnVeelkt",
+  COOKIE_KEY: 'secret'
+}
