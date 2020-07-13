4c3ed6328f59ad1421deeb84f14c62419e5e4676
Daylan <daylanberry@gmail.com>
2020-07-13 11:03:14 -0700

    config for deploy

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ require('./services/google.js')
 
 const app = express()
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression())
+  app.use(enforce.HTTPS({
+    trustProtoHeader: true
+  }))
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
+
+
 app.use(bodyParser.json())
 app.use('*', cors({ origin: 'http://localhost:3000', credentials: true }));
 
