144fca6682b5cd02a0390714af69c1a3e1a4cecd
Daylan <daylanberry@gmail.com>
2020-07-14 13:28:56 -0700

    working on configuring deployment

--- keys/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleSecretKey: process.env.GOOGLE_SECRET_KEY,
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
-}
