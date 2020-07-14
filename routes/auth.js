38d25cee3507dbd8a6b74eeb237a7e3d8a910792
Daylan <daylanberry@gmail.com>
2020-07-14 14:24:52 -0700

    working on configuring deployment

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,8 @@ module.exports = (app) => {
   app.get('/auth/google/callback',
     passport.authenticate('google', {
       failureRedirect: 'auth/google/failure',
-      successRedirect: '/'
+      successRedirect: '/',
+      session: true
     })
   )
 
